c3778d5d0036d295bb4f1005da8bae0dd0464b69
Chuxin Wang <156055853+Gracie-CXW@users.noreply.github.com>
2025-05-23 16:19:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
-# In progress. 
-# As of MAY-09-2025: Cleaned up initialization, removed messy and errornoeus sections. Developing tools for the AI. AI flow constructed. 
+# MAY-23-2025-FRIDAY==========================================================================================
+from langchain_community.llms import LlamaCpp
+from langgraph.graph import StateGraph, START, END
+from langchain_core.messages import AIMessage, HumanMessage
+from langgraph.prebuilt import tools_condition, ToolNode
+from typing_extensions import TypedDict, List, Literal
+from langchain_core.messages import AnyMessage
+from langgraph.graph.message import add_messages
+from langchain_core.documents import Document
 
-# This code is the core executable file for the MAMTOX AI 
-# The model chosen for initial demonstration of the project is DeepSeek R1 1.5B, imported from huggingface 
-# Ctrl + I for help
-
-# MAY-21-2025, WED
-# Initialization 
+# Initialization --------------------------------------------------------------------------------------------
 pdf_fp = r'' # put pdf here
 question = "" # put question here
 keywords = [] # put list of keywords (str) here
-
 vectordb_dir = r'vectordb/'
 cache_dir = r'cache/'
 
-# Define the local model
+# Define the local model -------------------------------------------------------------------------------------
 llm = Llamacpp(
     model_path = r'models/DeepSeek-R1-Distill-Qwen-14B/DeepSeek-R1-Distill-Qwen-14B-Q6_K.gguf',
     n_ctx = 4096,
@@ -24,42 +25,108 @@ llm = Llamacpp(
     verbose = False
 )
 
-# Build the AI pipeline
-import langgraph 
-from langgraph.graph import StateGraph, START, END
-from langchain_core.messages import AIMessage, HumanMessage
-# from langgraph.graph import MessagesState 
-from typing_extensions import TypedDict
-from utils import search_and_parse, just_parse
-from langgraph.prebuilt import tools_condition, ToolNode
-from typing import Annotated
-from langchain_core.messages import AnyMessage
-from langgraph.graph.message import add_messages
-
-class MessagesState(TypedDict):
+# Initialize State Schemas ---------------------------------------------------
+class GraphState(TypedDict):
+    question:str
+    context:List[Documents]
+    tool_result:str
+    output:str 
     messages: Annotated[list[AnyMessage], add_messages]
+    vector_store: Any 
+    #vector_stores: dict # for multiple pdfs. { pdf_fp: vector_store }
 
-llm_with_tools = llm.bind_tools([search_and_parse, just_parse]) # put tools here
-def tools_call_1(state:MessagesState):
-    return {"messages":[llm_with_tools.invoke(state["messages"])]}
+# Define Nodes ----------------------------------------------------------------
+llm_with_tools = llm.bind_tools([]) # put tools here
+def tools_call_1(state:GraphState):
+    return {"messages":[llm_with_tools.invoke(state["question"])]}
+
+tool_node_1 = ToolNode(tools=[]) # tools here
+
+def retrieve(state: GraphState):
+    if state['vector_store'] is None:
+        raise ValueError("Vector store not initialized")
+    retrieved_docs = state['vector_store'].similarity_search(state["question"])
+
+    ''' 
+    # For multiple pdfs
+    vector_store = state['vector_stores'].get(pdf_fp)
+    if vector_store is None:
+        raise ValueError('Vector store is not initialized')
+    retrieved_docs = vector_store.similarity_search(state['question'])
+    '''
+
+    return {'context': retrieved_docs}
+
+def augment(state:GraphState):
+    texts = '\n\n'.join(doc.page_content for doc in GraphState['context'])
+    question = state['question']
+    input_text = f"You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. Question: {question} \n Context: {texts}"
+    augmented_input = HumanMessage(content=input_text,name='Human')
+
+    return{'question':augmented_input}
+
+def generate(state:GraphState):
+    output = llm.invoke(state['question'])
+    conversation_history = [
+        HumanMessage(state['question'],name='Human'),
+        AIMessage(state['output'],name='D.A.T.A')
+    ]
+    return{
+        'output':output,
+        'messages':conversation_history
+    }
+
+# AI Router chooses Parsing function----------------------------
+'''
+llm_parse_tools = llm.bind_tools([search_and_parse,just_parse])
+def parsing_tool_call(state:GraphState):
+    return{"messages":[llm_parse_tools.invoke(state["question"])]}
+
+tool_parse = ToolNode(tools=[search_and_parse,just_parse])
+'''
+# Manual choosing of Parsing function -------------------------
+def parse_snp(state:GraphState):
+    search_and_parse(pdf_dir,keywords,vector_dir)
+    return {'vector_store':vector_store}
+    #return {'vector_stores': {pdf_fp:vector_store}} # for multiple pdfs
+
+def parse_all(state:GraphState):
+    just_parse(pdf_dir,vector_dir)
+    return{'vector_store':vector_store}
+    #return {'vector_stores': {pdf_fp:vector_store}} # for multiple pdfs
 
 
-# Build Graph
-builder = StateGraph(MessagesState)
-builder.add_node("tools_call_1", tools_call_1) # node to call tool uses, no output answer
-builder.add_edge(START,'tools_call_1')
-builder.add_node('tools_use_1',ToolNode([search_and_parse, just_parse])) # put tools here, actually outputs answer
-builder.add_conditional_edges("tools_call_1",tools_condition)
-builder.add_edge("tools_use_1",END)
+def snp_condition(state:GraphState) -> Literal('search_and_parse','just_parse'):
+    if keywords[0] is not None:
+        return 'search_and_parse'
+    elif keywords[0] is None:
+        return 'just_parse'
+
+#-----------------------------------------------------Build Graph ---------------------------------------
+builder = StateGraph(GraphState)
+# Nodes
+builder.add_node('search_and_parse',parse_snp)
+builder.add_node('just_parse',parse_all)
+builder.add_node("retriever_1",retrieve)
+builder.add_node('augment_1',augment)
+builder.add_node('tool_call_1',tools_call_1)
+builder.add_node('tool_use_1',tool_node_1)
+builder.add_node('generate_1',generate)
+
+#Edges
+builder.add_conditional_edges(START, snp_condition, {
+    'search_and_parse': 'search_and_parse',
+    'just_parse': 'just_parse'
+})
+builder.add_edge('search_and_parse', 'retriever_1')
+builder.add_edge('just_parse', 'retriever_1')
+builder.add_edge('retriever_1', 'augment_1')
+builder.add_edge('augment_1', 'tool_call_1')
+builder.add_conditional_edges('tool_call_1', tools_condition)
+builder.add_edge('tool_use_1', 'generate_1')
+builder.add_edge('generate_1', END)
 
 graph = builder.comile()
 
-# Messages and inputs
-messages_first = [HumanMessage(content = "", name='Human')] # put input here
+print(GraphState['messages'])
 
-messages_to_add = [HumanMessage(content = "Keywords for you to parse the pdf into relevant sections to answer the input question: ")]
-add_messages(messages_first,messages_to_add)
-
-messages = graph.invoke({"messages":messages}) # Gathers first AI response. 
-
-messages['messages'][-1].pretty_print() # Prints first Ai resopnse? 
